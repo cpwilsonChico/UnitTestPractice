d36eeaef0a16066f85e1cae59dc0f7d09027df85
cpwilsonChico <cpwilson@mail.csuchico.edu>
2020-04-20 14:43:39 -0700

    Expand test suite for palindromes

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,47 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+TEST(PracticeTest, is_palindrome_simple)
 {
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_not_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("abc");
+    ASSERT_FALSE(actual);
+}
+TEST(PracticeTest, is_medium_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("abcdefgfedcba");
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_palindrome_spaces)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("was it a rat i saw");
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_palindrome_nonalphanumeric)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a_bb_a");
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_palindrome_numeric)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("1614161");
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, is_not_palindrome_numeric)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("884788");
+    ASSERT_FALSE(actual);
+}
+
